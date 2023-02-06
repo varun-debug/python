394d7456d4de729423a7ed0ed28337047f6a83bd
Varun <53306722+varun-debug@users.noreply.github.com>
2023-02-06 21:00:20 +0530

    Create python_operations.py

---
+++ python_operations.py
@@ -0,0 +1,12 @@
+#Arithemetic operations
+x=5
+y=3
+print("Addition: ",(x+y))
+print("Subtraction: ",(x-y))
+print("Multiplication: ",(x*y))
+print("Float Division: ",(x/y))
+print("Interger Division: ",(x//y))
+print("Modulus: ",(x%y))
+print("Power of y on x: ",(x**y))
+
+#Some operations on String
