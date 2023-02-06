9652e6469e07792c4d9315e53eac41172ec53a7d
Varun <53306722+varun-debug@users.noreply.github.com>
2023-02-06 20:59:37 +0530

    Create python_if_else.py

---
+++ python_if_else.py
@@ -0,0 +1,16 @@
+x=5
+y=5
+if x==y:
+    print("Equals")
+else:
+    print("Not equal")
+
+a=60
+if a>=90:
+    print("Grade A")
+elif a<90 and a>70:
+    print("Grade B")
+elif a<70 and a>50:
+    print("Grade C")
+else:
+    print("Fail")
