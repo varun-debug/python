09c42d7c616dfcb636292b72bd311b66b55c9002
Varun <53306722+varun-debug@users.noreply.github.com>
2023-02-06 20:58:05 +0530

    Create python_start.py

---
+++ python_start.py
@@ -0,0 +1,24 @@
+int_var= 10
+float_var=1.1
+string_var= "varun"
+bool_var= True
+print('--------------value-------------')
+print('',int_var,'\n',float_var,'\n',string_var,'\n',bool_var)
+print('--------------Types-------------')
+print('type of int_var',type(int_var))
+print('type of float_var',type(float_var))
+print('type of string_var',type(string_var))
+print('type of bool_var',type(bool_var))
+print('--------------end---------------')
+
+#typecast
+int_var= 12 + 10
+casted_int_var=float(int_var)
+print("TypeCasting from int to float: ",casted_int_var)
+
+#input in Python
+name = input("enter your name= ")
+age = input("enter your age= ")
+print("User name= ",name)
+print("User age= ",age)
+
